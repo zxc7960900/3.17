64de9fd13d9d085e99bcb1154290d8a1fe79429f
zxc7960900 <zxc7960900@yahoo.com>
2017-10-08 20:56:20 +0800

    asdf

---
+++ source/main.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int accountnumber;
+	float beginningbalance;
+	float totalcharges;
+	float totalcredits;
+	float creditlimit;
+	float Balance;
+
+	printf("Enter account number (-1 to end):");
+	scanf_s("%d", &accountnumber);
+
+	while (accountnumber != EOF)
+	{
+		printf("Enter beginning balance:");
+		scanf_s("%f", &beginningbalance);
+
+		printf("Enter total charges:");
+		scanf_s("%f", &totalcharges);
+
+		printf("Enter total credits:");
+		scanf_s("%f", &totalcredits);
+
+		printf("Enter credit limit:");
+		scanf_s("%f", &creditlimit);
+		printf("\n");
+
+		Balance = beginningbalance + totalcharges - totalcredits;
+
+		if (Balance > creditlimit)
+		{
+			printf("Account:	%d\n", accountnumber);
+			printf("Credit limit:	%.2f\n", creditlimit);
+			printf("Balance:	%.2f\n", Balance);
+			printf("Credit Limit Exceeded.\n\n");
+			printf("Enter account number (-1 to end):");
+			scanf_s("%d", &accountnumber);
+		}
+
+		else
+		{
+			printf("Enter account number (-1 to end):");
+			scanf_s("%d", &accountnumber);
+		}
+
+	}
+	system("pasue");
+	return 0;
+}
